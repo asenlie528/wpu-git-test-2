f85e42523c7d4063f47a7e008a293fbfa489ed82
Harto Lie Sudianto <asens_b8@yahoo.com>
2020-04-28 20:54:10 +0900

    Tambah file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique perspiciatis sit, tenetur nemo in maiores? Dolorem, ut! Veritatis illum eum voluptas praesentium maxime ab consectetur nesciunt beatae facere totam! Facilis.</p>
+</body>
+</html>
